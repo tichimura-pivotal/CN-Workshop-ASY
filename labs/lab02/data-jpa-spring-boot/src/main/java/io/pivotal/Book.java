c50204d38e791e6fecb01c088fd5285fdc3a4b1d
phopper-pivotal <phopper@pivotal.io>
2016-04-23 21:08:18 -0600

    added new files

--- labs/lab02/data-jpa-spring-boot/src/main/java/io/pivotal/Book.java
+++ labs/lab02/data-jpa-spring-boot/src/main/java/io/pivotal/Book.java
@@ -18,18 +18,22 @@ public class Book {
 	private String description;
 	
 	public Long getId() {
+
 		return id;
 	}
 
 	public void setId(Long id) {
+
 		this.id = id;
 	}
 
 	public String getReader() {
+
 		return reader;
 	}
 
 	public void setReader(String reader) {
+
 		this.reader = reader;
 	}
 
@@ -38,31 +42,38 @@ public class Book {
 	}
 	
 	public void setIsbn(String isbn) {
+
 		this.isbn = isbn;
 	}
 	
 	public String getTitle() {
+
 		return title;
 	}
 	
 	public void setTitle(String title) {
+
 		this.title = title;
 	}
 	
 	public String getAuthor() {
+
 		return author;
 	}
 	
 	public void setAuthor(String author) {
+
 		this.author = author;
 	}
 	
 	public String getDescription() {
+
 		return description;
 	}
 	
 	public void setDescription(String description) {
+
 		this.description = description;
 	}
-	
+
 }
